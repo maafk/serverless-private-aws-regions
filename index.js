af8ae41b6ab45a5f1454799b6389720c2b541e4f
maafk <maafk@users.noreply.github.com>
2019-10-01 20:01:14 -0400

    Closes #1 add relevatnt lifecycle hooks for commands that reach out to aws

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ class ServerlessPrivateAWSRegions {
       'before:deploy:deploy': this.prepRegion.bind(this),
       'before:remove:remove': this.prepRegion.bind(this),
       'before:deploy:function:initialize': this.prepRegion.bind(this),
+      'before:invoke:invoke': this.prepRegion.bind(this),
+      'before:info:info': this.prepRegion.bind(this),
+      'before:rollback:initialize': this.prepRegion.bind(this),
+      'before:logs:logs': this.prepRegion.bind(this),
       'after:aws:package:finalize:mergeCustomProviderResources': this.updatePrincipals.bind(
         this
       )
